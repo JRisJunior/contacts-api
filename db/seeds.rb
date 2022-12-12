abf4534cc94463be6842c73dedb41aa623fd5325
JRisJunior <jeffmrubin@gmail.com>
2022-12-12 11:34:02 -0600

    after address column  migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,13 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-100.times do
-  contact = Contact.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, phone_number: Faker::PhoneNumber.phone_number)
+1.times do
+  contact = Contact.new(first_name: "TEST", last_name: "ADDRESS", email: Faker::Internet.email, phone_number: Faker::PhoneNumber.phone_number, address: "1060 W Addison St, Chicago, IL 60613" )
 contact.save
 end
+
+
+# 1.times do
+#   contact = Contact.new(first_name: "Faker::Name.first_name", last_name: Faker::Name.last_name, email: Faker::Internet.email, phone_number: Faker::PhoneNumber.phone_number, )
+# contact.save
+# end
